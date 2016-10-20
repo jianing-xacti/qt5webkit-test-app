40e8ade58cb302b8efc0a1439e5f6108bac00cfe
bijianing <jianing.bi@xacti-co.com>
2016-10-20 11:40:36 +0900

    add url processing

--- main.cpp
+++ main.cpp
@@ -3,10 +3,20 @@
 
 int main(int argc, char *argv[])
 {
+	char *def_url = "./test.html";
+	char url[128];
+
     QApplication a(argc, argv);
     QWebView view;
     view.show();
 //    view.load(QUrl("http://google.com"));
-	view.load(QUrl("./test.html"));
+
+	strcpy(url, "file:///");
+	if (argc == 2)
+		strcat(url, argv[1]);
+	else
+		strcat(url, def_url);
+
+	view.load(QUrl(url));
     return a.exec();
 }
